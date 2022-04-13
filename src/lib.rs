467033b7395c0e941ce5c1f6ff756744bb27a312
Jens Reimann <jreimann@redhat.com>
2022-04-13 10:33:11 +0200

    refactor: pull oauth2 and openid on the crate root level

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,20 @@ pub mod components;
 pub mod config;
 pub mod context;
 pub mod prelude;
+
+#[cfg(feature = "openid")]
+pub mod openid {
+    pub use crate::components::context::openid::*;
+    pub use crate::components::redirect::location::openid::*;
+    #[cfg(feature = "yew-router-nested")]
+    pub use crate::components::redirect::router::openid::*;
+    pub use crate::config::openid::*;
+}
+
+pub mod oauth2 {
+    pub use crate::components::context::oauth2::*;
+    pub use crate::components::redirect::location::oauth2::*;
+    #[cfg(feature = "yew-router-nested")]
+    pub use crate::components::redirect::router::oauth2::*;
+    pub use crate::config::oauth2::*;
+}
--- src/prelude.rs
+++ src/prelude.rs
@@ -5,19 +5,6 @@ pub use crate::components::*;
 pub use crate::config::*;
 pub use crate::context::*;
 
+pub use crate::oauth2;
 #[cfg(feature = "openid")]
-pub mod openid {
-    pub use crate::components::context::openid::*;
-    pub use crate::components::redirect::location::openid::*;
-    #[cfg(feature = "yew-router-nested")]
-    pub use crate::components::redirect::router::openid::*;
-    pub use crate::config::openid::*;
-}
-
-pub mod oauth2 {
-    pub use crate::components::context::oauth2::*;
-    pub use crate::components::redirect::location::oauth2::*;
-    #[cfg(feature = "yew-router-nested")]
-    pub use crate::components::redirect::router::oauth2::*;
-    pub use crate::config::oauth2::*;
-}
+pub use crate::openid;
