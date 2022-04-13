5fb9d659fbbd5b97e0390044f1c610061ba5dad0
Jens Reimann <jreimann@redhat.com>
2022-04-13 14:13:03 +0200

    feat: make it easier to work with context values

--- src/context.rs
+++ src/context.rs
@@ -32,9 +32,9 @@ pub enum OAuth2Context {
 
 impl OAuth2Context {
     /// Get the access token, if the context is [`OAuth2Context::Authenticated`]
-    pub fn access_token(&self) -> Option<String> {
+    pub fn access_token(&self) -> Option<&str> {
         match self {
-            Self::Authenticated { access_token, .. } => Some(access_token.clone()),
+            Self::Authenticated { access_token, .. } => Some(&access_token),
             _ => None,
         }
     }
@@ -129,5 +129,87 @@ where
         F: Fn(OAuth2Context) -> Self::Message + 'static,
     {
         self.unzipped_with(self.callback(f))
+    }
+}
+
+pub trait AsContextValue {
+    type Message;
+
+    fn as_value<T, F>(&self, f: F) -> ContextValue<T>
+    where
+        T: 'static + Clone + PartialEq,
+        F: Fn(T) -> Self::Message + 'static;
+}
+
+impl<C> AsContextValue for Scope<C>
+where
+    C: Component,
+{
+    type Message = C::Message;
+
+    fn as_value<T, F>(&self, f: F) -> ContextValue<T>
+    where
+        T: 'static + Clone + PartialEq,
+        F: Fn(T) -> Self::Message + 'static,
+    {
+        self.context::<T>(self.callback(f)).into()
+    }
+}
+
+impl<C> AsContextValue for Context<C>
+where
+    C: Component,
+{
+    type Message = C::Message;
+
+    fn as_value<T, F>(&self, f: F) -> ContextValue<T>
+    where
+        T: 'static + Clone + PartialEq,
+        F: Fn(T) -> Self::Message + 'static,
+    {
+        self.link().as_value(f)
+    }
+}
+
+/// A helper which holds both value and handle in a way that it can easily be updated if it
+/// is present.
+pub enum ContextValue<T>
+where
+    T: 'static + Clone + PartialEq,
+{
+    Some(T, ContextHandle<T>),
+    None,
+}
+
+impl<T> From<Option<(T, ContextHandle<T>)>> for ContextValue<T>
+where
+    T: 'static + Clone + PartialEq,
+{
+    fn from(value: Option<(T, ContextHandle<T>)>) -> Self {
+        match value {
+            Some(value) => Self::Some(value.0, value.1),
+            None => Self::None,
+        }
+    }
+}
+
+impl<T> ContextValue<T>
+where
+    T: 'static + Clone + PartialEq,
+{
+    /// Set a new value, only if the handle is present.
+    pub fn set(&mut self, new_value: T) {
+        match self {
+            Self::Some(value, _) => *value = new_value,
+            Self::None => {}
+        }
+    }
+
+    /// Get the current value.
+    pub fn get(&self) -> Option<&T> {
+        match &self {
+            Self::Some(value, _) => Some(value),
+            Self::None => None,
+        }
     }
 }
