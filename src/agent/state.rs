429287eedf6301ba42f121cb61bb6790197a5388
Alexandre Roba <alexandre.roba@arobanet.be>
2024-01-25 11:26:48 +0100

    Resolve conflict

--- src/agent/state.rs
+++ src/agent/state.rs
@@ -1,11 +1,7 @@
 pub const STORAGE_KEY_CSRF_TOKEN: &str = "ctron/oauth2/csrfToken";
 pub const STORAGE_KEY_LOGIN_STATE: &str = "ctron/oauth2/loginState";
 pub const STORAGE_KEY_REDIRECT_URL: &str = "ctron/oauth2/redirectUrl";
-<<<<<<< HEAD
 pub const STORAGE_KEY_POST_LOGIN_URL: &str = "ctron/oauth2/postLoginUrl";
-=======
-pub const STORAGE_KEY_CURRENT_URL: &str = "ctron/oauth2/currentUrl";
->>>>>>> 6abf190 (Resolved conflict)
 
 #[derive(Debug)]
 pub struct State {
